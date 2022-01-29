280e5df48d65ea2feaacbdfd7187f31e97001ce5
SANTHOSHPODILIGITHHUB <98577065+SANTHOSHPODILIGITHHUB@users.noreply.github.com>
2022-01-29 17:23:37 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -9,49 +9,54 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(command("start") & filters.private & ~filters.group & ~filters.edited)
 async def start_(client: Client, message: Message):
-    await message.reply_sticker("CAACAgQAAxkBAAEDsTZh4xBVu96tWo0G0CIbn_meSGs6LwACWxcAAqbxcR4yeTJRtPe4UCME")
+    await message.reply_sticker("CAACAgQAAxkBAAIC_mH1JUrL_s4kgKA5hiDk_Rrl0GYWAAIeCgACz9YRUXNuChP5kGjfIwQ")
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/f6f020ef5aeeb079b15b9.jpg",
-        caption=f"""**━━━━━━━━━━━━━━━━━━━━━━
-🖤 ʜᴇʏ, ɪ ᴀᴍ sᴜᴘᴇʀ ғᴀsᴛ ᴠᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs...
-ᴀʟʟ ᴏꜰ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ : /
-┏━━━━━━━━━━━━━━━━━┓
-┣★ ᴄʀᴇᴀᴛᴏʀ : [𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦 🖤 𝗕𝗢𝗬](t.me/anonymous_was_bot)
-┗━━━━━━━━━━━━━━━━━┛
+        photo=f"https://te.legra.ph/file/ffbb096d10dd36ad45337.jpg",
+        caption=f"""**━━━━━━━━━━━━ 🌺🌻🌹🌷━━━━━━━━━━
+━━━━━━━━━━━━━━━━━━━━━━
+😊ʜɪ ɪᴀᴍ ᴛᴇʟᴇɢʀᴀᴍ ᴠᴏɪᴄᴇ ᴍᴜsɪᴄ ʙᴏᴛ... ᴅᴇᴘʟᴏʏ ʙʏ : @santhu_music_bot
 
-💞 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ ᴅᴍ ᴛᴏ ᴍʏ [ᴏᴡɴᴇʀ](t.me/anonymous_was_bot) ʙᴀʙʏ...
+┏━━━━━━━━━━━━━━━━━┓ 🌺🌻🌹🌷🌺🌻🌹🌷━━━━━━━━━━━━━━━━━━━━━━
+┣» ᴏᴘ ᴍᴜꜱɪᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ. 
+┣» ʜɪɢʜ ǫᴜᴀʟɪᴛʏ  ᴍᴜꜱɪᴄ.
+┣» ᴀᴅᴠᴀɴᴄᴇᴅ ꜰᴇᴀᴛᴜʀᴇꜱ.
+┣» ꜱᴜᴘᴇʀꜰᴀꜱᴛ ꜱᴘᴇᴇᴅ. 
+┣» [𝐃𝐄𝐏𝐋𝐎𝐘 𝐁𝐘 ❤️](https://t.me/santhu_music_bot)
+┗━━━━━━━━━━━━━━━━━┛
+[𝐎𝐖𝐍𝐄𝐑 ❤️](https://t.me/santhu_music_bot)
+𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [𝐒𝐀𝐍𝐓𝐇𝐔❤️](https://t.me/santhu_music_bot)**""",
 ━━━━━━━━━━━━━━━━━━━━━━**""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "😫ᴀᴅᴅ ᴍᴇ ᴇʟsᴇ ʏᴏᴜ ɢᴇʏ​😫", url="https://t.me/fallen_music_bot?startgroup=true")
+                        "💞sᴀɴᴛʜᴜ ɴɪ ᴀᴅᴅ ᴄʜᴇsᴜᴋᴏɴᴅɪ💞", url="https://t.me/Santhuadvancefreemusicbot?startgroup=true")
                   ],[
                     InlineKeyboardButton(
-                        "😘ᴄʀᴇᴀᴛᴏʀ😘", url="https://t.me/anonymous_was_bot"
+                        "☹️ᴏᴡɴᴇʀ😘", url="https://t.me/santhu_music_bot"
                     ),
                     InlineKeyboardButton(
-                        "💔sᴜᴘᴘᴏʀᴛ💔", url="https://t.me/DevilsHeavenMF"
+                        "😇ɢʀᴏᴜᴘ💞", url="https://t.me/santhuvc"
                     )
                 ],[ 
                     InlineKeyboardButton(
-                        "🤔sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​🤔", url="https://t.me/DevilsHeavenMF"
+                        "😁NETWORK😊", url="https://t.me/santhubotupadates"
                     )]
             ]
        ),
     )
 
-@Client.on_message(command(["ping"]) & filters.group & ~filters.edited & ~filters.private)
+@Client.on_message(command(["repo"]) & filters.group & ~filters.edited & ~filters.private)
 
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/93b08120e36d096cd21ca.jpg",
-        caption=f"""ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ !🖤""",
+        photo=f"https://te.legra.ph/file/ffbb096d10dd36ad45337.jpg",
+        caption=f"""ᴘʀɪᴠᴀᴛᴇ ʀᴇᴘᴏ !🖤""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "💕sᴜᴘᴘᴏʀᴛ💕", url=f"https://t.me/DevilsHeavenMF")
+                        "💕ᴏᴡɴᴇʀ💕", url=f"https://t.me/santhu_music_bot")
                 ]
             ]
         ),
